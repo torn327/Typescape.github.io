e0eec3066fe763d191e586f2e264c017cfa454ad
torn327 <torn327anin000@GMAIL.COM>
2025-03-24 12:55:56 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ wss.on('connection', (ws) => {
 app.use(express.static('public'));  // Make sure you have a 'public' folder with your frontend files
 
 // Start the server
-const port = process.env.PORT || 3000;  // Make sure the port is correct for your environment (Render might change this)
+const port = process.env.PORT || 10000;  // Make sure the port is correct for your environment (Render might change this)
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
