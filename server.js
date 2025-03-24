1aa5d22fb20a34d41664849e4e2053813812a1f9
torn327 <torn327anin000@GMAIL.COM>
2025-03-24 13:40:31 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,24 +7,58 @@ const server = http.createServer(app);  // Use http server instead of https
 const wss = new WebSocket.Server({ server });
 
 let connections = [];
+let characters = generateInitialCharacters();  // Store the characters in memory
+
+// Generate an initial set of characters (you can adjust this as needed)
+function generateInitialCharacters() {
+    const characters = [];
+    const allChars = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5',
+        '6', '7', '8', '9', '0', '`', '-', '=', '[', ']', '\\',
+        ';', "'", ',', '.', '/', '!', '@', '#', '$', '%', '^',
+        '&', '*', '(', ')', '_', '+', '{', '}', '|', ':', '"',
+        '<', '>', '?', '~', ' ', '\n'
+    ];
+
+    for (let i = 0; i < 8320; i++) {
+        characters.push(allChars[i % allChars.length]);  // Repeat characters as needed
+    }
+    return characters;
+}
 
 // WebSocket connection handler
 wss.on('connection', (ws) => {
     console.log('New client connected');
     connections.push(ws);
 
+    // Send the initial state to the newly connected client
+    ws.send(JSON.stringify({ type: 'initial', characters }));
+
     // When a message is received from a client
     ws.on('message', (message) => {
         console.log('Received message: ' + message);
+        const data = JSON.parse(message);
 
-        // Broadcast the message to all other clients
-        connections.forEach(client => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+        if (data.type === 'update') {
+            // Update the character on the server
+            characters[data.index] = data.char;
+
+            // Broadcast the updated character to all other clients
+            connections.forEach(client => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        type: 'update',
+                        index: data.index,
+                        char: data.char
+                    }));
+                }
+            });
+        }
     });
 
+    // Handle client disconnection
     ws.on('close', () => {
         // Remove disconnected client from the list
         connections = connections.filter(client => client !== ws);
